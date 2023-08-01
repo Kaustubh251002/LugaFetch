294777db7c79535758907d204b6639dc32ab736a
Kaustubh251002 <kmishra2510@gmail.com>
2023-08-02 03:27:24 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-red-700 text-bold text-3xl"></div>
+    </div>
+  );
+}
+
+export default App;
+q
